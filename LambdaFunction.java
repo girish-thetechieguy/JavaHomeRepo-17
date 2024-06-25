2fb28dd86134e60818ec20b3c03ae0b38f03dcff
girish.balekundri <girish.thetechieguy@gmail.com>
2024-06-25 12:53:19 +0800

    InitCommit

---
+++ LambdaFunction.java
@@ -0,0 +1,9 @@
+/**
+ * LambdaFunction
+ */
+public class LambdaFunction {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java versions comparison!!");
+    }
+}
